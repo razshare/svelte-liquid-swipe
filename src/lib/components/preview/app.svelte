05012451444de90bf78fa6d7b48993affd2a5396
Razvan Tanase <tangent.jotey@gmail.com>
2023-04-02 03:59:41 +0200

    fixing preview

--- src/lib/components/preview/app.svelte
+++ src/lib/components/preview/app.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import LiquidContainer from '$lib/components/liquid-container.svelte'
-  import Page1 from '$lib/components/example/page1.svelte'
-  import Page2 from '$lib/components/example/page2.svelte'
-  import Page3 from '$lib/components/example/page3.svelte'
-  import Page4 from '$lib/components/example/page4.svelte'
+  import Page1 from '$lib/components/preview/page1.svelte'
+  import Page2 from '$lib/components/preview/page2.svelte'
+  import Page3 from '$lib/components/preview/page3.svelte'
+  import Page4 from '$lib/components/preview/page4.svelte'
 
   const pages = [Page1, Page2, Page3, Page4]
 </script>
