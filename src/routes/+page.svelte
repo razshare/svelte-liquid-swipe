05ab67d7da0c1ff3715862aec5a86bca8a855071
Razvan Tanase <tangent.jotey@gmail.com>
2023-04-02 22:05:02 +0200

    Using fullscreen for a better preview on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,6 @@
 </script>
 
 
-<div style="position:relative;width:600px;height:800px;overflow:hidden">
+<div style="position:fixed;left:0;right:0;top:0;bottom:0;overflow:hidden">
     <LiquidContainer direction="right" components={pages}/>
 </div>
